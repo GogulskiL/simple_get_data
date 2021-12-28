df9a29ef69a8f51b04859482f8b3077af21e774d
łukasz gogulski <lukaszgogul@gmail.com>
2021-12-28 22:31:46 +0100

    refactoring continues

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from openpyxl import Workbook
 
 # download the page
 url = 'https://rpa.hybrydoweit.pl'
+title = "Tytuł"
+industry = "Branża/dział"
+link = "link"
 
 
 def get_data(url):
@@ -27,28 +30,43 @@ def get_article_data_reverse(url):
 
 
 # create excel sheet
-wb = Workbook()
-sheet = wb.active
-sheet1 = wb.create_sheet("Sheet1")
+# wb = Workbook()
+# sheet = wb.active
+# sheet1 = wb.create_sheet("Sheet1")
 
-sheet.cell(row=1, column=1).value = "Tytuł"
-sheet.cell(row=1, column=2).value = "Branża/dział"
-sheet.cell(row=1, column=3).value = "Link"
+# sheet.cell(row=1, column=1).value = "Tytuł"
+# sheet.cell(row=1, column=2).value = "Branża/dział"
+# sheet.cell(row=1, column=3).value = "Link"
 
-sheet1.cell(row=1, column=1).value = "Tytuł"
-sheet1.cell(row=1, column=2).value = "Branża/dział"
-sheet1.cell(row=1, column=3).value = "Link"
+# sheet1.cell(row=1, column=1).value = "Tytuł"
+# sheet1.cell(row=1, column=2).value = "Branża/dział"
+# sheet1.cell(row=1, column=3).value = "Link"
 
-data_export = get_article_data(url)
-data_rev = get_article_data_reverse(url)
-# iterate over the elements and add them to the sheet
-for i in range(2, 7):
-    for k in range(0, 3):
-        sheet.cell(row=i, column=k+1).value = data_export[i-2][k]
+# data_export = get_article_data(url)
+# data_rev = get_article_data_reverse(url)
+# # iterate over the elements and add them to the sheet
+# for i in range(2, 7):
+#     for k in range(0, 3):
+#         sheet.cell(row=i, column=k+1).value = data_export[i-2][k]
 
-for i in range(2, 7):
-    for k in range(0, 3):
-        sheet1.cell(row=i, column=k+1).value = data_rev[i-2][k]
+# for i in range(2, 7):
+#     for k in range(0, 3):
+#         sheet1.cell(row=i, column=k+1).value = data_rev[i-2][k]
 
-# save sheet
-wb.save("excel_file.xlsx")
+# # save sheet
+# wb.save("excel_file.xlsx")
+def create_excel_file(name_cell_1, name_cell_2, name_cell_3):
+    wb = Workbook()
+    sheet = wb.active
+    sheet1 = wb.create_sheet("Sheet1")
+    sheet.cell(row=1, column=1).value = name_cell_1
+    sheet.cell(row=1, column=2).value = name_cell_2
+    sheet.cell(row=1, column=3).value = name_cell_3
+
+    sheet1.cell(row=1, column=1).value = name_cell_1
+    sheet1.cell(row=1, column=2).value = name_cell_2
+    sheet1.cell(row=1, column=3).value = name_cell_3
+    wb.save("data_file.xlsx")
+
+def fill_excel_file():
+    pass
